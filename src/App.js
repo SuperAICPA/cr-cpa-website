0539e34390eaa236dd76695daf41c4846e2ac541
Danny Kim <kcr720309@gmail.com>
2026-03-23 16:52:13 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import ReviewsSection from './components/ReviewsSection';
 import CRAccountancy from './cpa-website';
 
 function App() {
   return <CRAccountancy />;
+    <ReviewsSection />
 }
 
 export default App;
