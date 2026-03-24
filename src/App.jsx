f88c7b268246e157e9ead6cc4e7d9137672615b0
Danny Kim <kcr720309@gmail.com>
2026-03-23 17:15:45 -0700

    Update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CPAWebsite from './cpa-website';
+import ReviewsSection from './components/ReviewsSection';
+
+function App() {
+  return (
+    <>
+      <CPAWebsite />
+      <ReviewsSection />
+    </>
+  );
+}
+
+export default App;
